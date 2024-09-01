b322dafe946a3d185003566f7e968031648fc8d9
emp3r0r7 <ichigochris@hotmail.it>
2024-09-01 23:15:06 +0200

    added js libraries locally , slight enhancement to realtime-data.html file, wip

--- src/main/java/org/emp3r0r7/controller/WebSocketController.java
+++ src/main/java/org/emp3r0r7/controller/WebSocketController.java
@@ -1,13 +1,25 @@
 package org.emp3r0r7.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebSocketController {
 
+
+    @Value("${frontend.websocket.endpoint.path}")
+    private String webSocketEndpoint;
+
+    @Value("${server.port}")
+    private Integer port;
+
     @GetMapping("/realtime-data")
-    public String getRealTimeDataPage() {
-        return "realtime-data";  // Nome del file HTML da servire
+    public String getRealTimeDataPage(Model model) {
+        // Aggiungi le proprietà al modello per Thymeleaf
+        model.addAttribute("webSocketEndpoint", webSocketEndpoint);
+        model.addAttribute("port", port);
+        return "realtime-data";
     }
 }
